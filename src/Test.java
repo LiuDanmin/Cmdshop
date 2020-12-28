d6efdac542cec302b323816e9051f616ffca652c
LiuDanmin <2433395370@qq.com>
2020-12-28 21:08:47 +0800

    2020-12-28

--- src/Test.java
+++ src/Test.java
@@ -1,29 +1,46 @@
-import java.io.File;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Test {
-    public static void main(String[] args) {
-        /*开始读文件*/
-        int k=0;
-        File file = new File("C:\\Users\\lenovo\\Desktop\\2020-12-27\\src\\user.xlsx");
-        ReadExcel readExcel =new ReadExcel();//创建对象
-        User users[]=readExcel.readExcel(file);
+    public static void main(String[] args) throws ClassNotFoundException {
+        boolean bo=true;
+        while (bo) {
+            System.out.println("请输入用户名：");
+            Scanner sc = new Scanner(System.in);
+            String username = sc.next();//阻塞方法
+            System.out.println("请输入密码：");
+            String password = sc.next();
 
-        System.out.println("请输入用户名：");
-        Scanner sc=new Scanner(System.in);
-        String username=sc.next();
+            //File file = new File("C:\\Users\\Administrator\\IdeaProjects\\ConsoleShop\\src\\users.xlsx");
+            InputStream in = Class.forName("Test").getResourceAsStream("/users.xlsx");
+            InputStream inProduct = Class.forName("Test").getResourceAsStream("/product.xlsx");
+            ReadExcel readExcel = new ReadExcel();//创建对象
+            User users[] = readExcel.readExcel(in);
+            for (int i = 0; i < users.length; i++) {
+                if (username.equals(users[i].getUsername()) && password.equals(users[i].getPassword())) {
+                    System.out.println("登录成功");
+                    bo=false;
 
-        System.out.println("请输入密码：");
-        String password=sc.next();
-        for(User user:users){
-            if(username.equals(user.getUsername()) && password.equals(user.getPassword())){
-                k=1;
+                    ReadProductExcel readProductExcel=new ReadProductExcel();
+                    //Product products[]=readProductExcel.readExcel(inProduct);
+                    Product products[] = readProductExcel.getAllProduct(inProduct);
+                    for(Product product:products){
+                        System.out.println(product.getpId());
+                        System.out.println("\t" + product.getpName());
+                        System.out.println("\t" + product.getPrice());
+                        System.out.println("\t" + product.getpDesc());
+                    }
+                    System.out.println("请输入商品ID把商品加入购物车");
+                    String pId=sc.next();
+
+                    //创建一个购物车数组：存的是商品
+                    Product carts[]=new Product[3];
+
+                    break;
+                } else {
+                    System.out.println("登录失败");
+                }
             }
-        }
-        if(k==1){
-            System.out.println("登陆成功");
-        }else{
-            System.out.println("登录失败");
         }
     }
 }
--- src/User.java
+++ src/User.java
@@ -4,15 +4,35 @@ public class User {
     private String address;
     private String phone;
 
-    public String getUsername(){return username;}
-    public void setUsername(String username){this.username=username;}
+    public String getUsername() {
+        return username;
+    }
 
-    public String getPassword(){return password;}
-    public void setPassword(String password){this.password=password;}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-    public String getAddress(){return address;}
-    public void setAddress(String address){this.address=address;}
+    public String getPassword() {
+        return password;
+    }
 
-    public String getPhone(){return phone;}
-    public void setPhone(String phone){this.phone=phone;}
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
